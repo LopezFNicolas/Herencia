48e38d52843a299297fecc1866c06739b8f7e765
NicolasF <lopezfn25@gmail.com>
2026-08-28 08:31:37 -0500

    main

--- src/Camion.java
+++ src/Camion.java
@@ -1,11 +1,4 @@
 public class Camion extends Vehiculos {
     private double capacidadCarga = 0;
 
-    public double getcapacidadCarga() {
-        return capacidadCarga;
-    }
 
-    public void setcapacidadCarga(double capacidadCarga) {
-        this.capacidadCarga = capacidadCarga;
-    }
-}
