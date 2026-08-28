3206783aeae0a06b423447379162689425cd0a0d
NicolasF <lopezfn25@gmail.com>
2026-08-28 08:26:27 -0500

    Clase Madre

--- src/Camion.java
+++ src/Camion.java
@@ -1,1 +1,11 @@
+public class Camion extends Vehiculos {
+    private double capacidadCarga = 0;
 
+    public double getcapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setcapacidadCarga(double capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+}
