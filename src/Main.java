11d0f6aa858f3b1720b21f5373f0c6721a37dc2c
NicolasF <lopezfn25@gmail.com>
2026-08-28 08:29:21 -0500

    main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Carro miCarro = new Carro();
+        miCarro.setmarca("Mazda");
+        miCarro.setmodelo("CX-5");
+        miCarro.setasientos(5);
+        miCarro.setllantas(4);
+        miCarro.setnumeroPuertas(4);
+
+        }
