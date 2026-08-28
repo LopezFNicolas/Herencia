9a5cdbfc02b9becfc291d1c3ab3eab28afbb372d
NicolasF <lopezfn25@gmail.com>
2026-08-28 08:28:19 -0500

    clase hija

--- src/Moto.java
+++ src/Moto.java
@@ -1,1 +1,12 @@
+public class Moto extends Vehiculos {
+    private boolean tieneSidecar = false;
+
+    public boolean gettieneSidecar() {
+        return tieneSidecar;
+    }
+
+    public void settieneSidecar(boolean tieneSidecar) {
+        this.tieneSidecar = tieneSidecar;
+    }
+}
 
