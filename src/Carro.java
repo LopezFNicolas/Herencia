85999015a86a42fb9846299a5d53ad94cd86c05c
NicolasF <lopezfn25@gmail.com>
2026-08-28 08:24:13 -0500

    Clase Madre

---
+++ src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro extends Vehiculos {
+    private int numeroPuertas = 4;
+
+    public int getnumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setnumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+}
