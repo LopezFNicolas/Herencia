cc90a528f349c2c0627087a9b2728d80f4e785f8
NicolasF <lopezfn25@gmail.com>
2026-08-28 08:21:05 -0500

    Clase Madre

---
+++ src/Vehiculos.java
@@ -0,0 +1,48 @@
+
+public class Vehiculos {
+    private String marca = "";
+    private int asientos = 0;
+    private String modelo = "";
+    private int llantas = 0;
+    private int cilindraje = 0;
+
+    public String getmarca() {
+        return marca;
+    }
+
+    public void setmarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getmodelo() {
+        return modelo;
+    }
+
+    public void setmodelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getasientos() {
+        return asientos;
+    }
+
+    public void setasientos(int asientos) {
+        this.asientos = asientos;
+    }
+
+    public int getllantas() {
+        return llantas;
+    }
+
+    public void setllantas(int llantas) {
+        this.llantas = llantas;
+    }
+
+    public int getcilindraje() {
+        return cilindraje;
+    }
+
+    public void setcilindraje(int cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+}
